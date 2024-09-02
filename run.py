cd6163cc3ca121024f0830f7120e3eb57bf3fb36
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-09-02 13:44:09 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -76,7 +76,18 @@ def  calculate_surplus_data(sales_row):
     print("Calculate surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    """
+    Use zip method to iterate to list at a same time in a one loop
+    """
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
+
+    
 
 
 def main():
@@ -86,7 +97,7 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
-
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 print("Welcome to love sandwiches data Automation")
 main()
